d207800cfe95ba148ed0ea54797d854cc4ff0018
steverob <stevanity.hf@gmail.com>
2012-09-30 14:09:38 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpcPcMgmt::Application.config.secret_token = '8f34df70ea4ef00d6b299008314f9559289836290aa2614088efd665208e84eb5db408cb360d2399ea35772ec5157db1ac23df5b7be14e15ce803e14733d4910'
